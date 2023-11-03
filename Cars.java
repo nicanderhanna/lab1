011e65d227eddc5d7b2576e03859f2091f4d00cd
David <riveron@chalmers.se>
2023-11-03 15:00:08 +0100

    flyttade över gemensamma metoder till superklassen Cars :)

---
+++ Cars.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+
+public class Cars {
+    public int nrDoors; // Number of doors on the car
+    public double enginePower; // Engine power of the car
+    public double currentSpeed; // The current speed of the car
+    public Color color; // Color of the car
+    public String modelName; // The car model name
+
+    public void setColor(Color clr){
+        color = clr;
+    }
+
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void stopEngine(){
+        currentSpeed = 0;
+    }
+
+}
